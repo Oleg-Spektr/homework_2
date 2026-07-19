7d2f2db7b6d2a29cf41207d58b0a09066167ae6f
имя фамилия < >
2026-07-19 11:28:37 +0300

    обновлён файл readme и отформатирован код по pep8

--- 14.1_main.py
+++ 14.1_main.py
@@ -4,9 +4,7 @@ from src.utils import read_json
 
 if __name__ == "__main__":
     # --- БЛОК 1: Проверка Заданий 1 и 2 (Исходный код из 14.1_main.py) ---
-    product1 = Product(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
-    )
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
--- 15.1_main.py
+++ 15.1_main.py
@@ -3,9 +3,7 @@ from src.product import Product
 from src.utils import read_json
 
 if __name__ == "__main__":
-    product1 = Product(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
-    )
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
